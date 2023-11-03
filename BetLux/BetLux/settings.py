b2fd5dd5c0709dc248f0fecee78b0b543491f973
andrea <ndrwrono2001@gmail.com>
2023-11-03 15:36:21 +0300

    changes to handle deployment on vercel

--- BetLux/BetLux/settings.py
+++ BetLux/BetLux/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('CUSTOM_SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -143,3 +143,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
