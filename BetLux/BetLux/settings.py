68ce91653287097e18a74794297df209aabbda80
andrea <ndrwrono2001@gmail.com>
2023-11-03 14:01:58 +0300

    conf

--- BetLux/BetLux/settings.py
+++ BetLux/BetLux/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('CUSTOM_SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://betblitz.onrender.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
