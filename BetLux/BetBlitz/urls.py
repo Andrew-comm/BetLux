dc58f8a45c712580ceab34004bdc671cdea8d486
andrea <ndrwrono2001@gmail.com>
2023-11-03 13:09:26 +0300

    updates

--- BetLux/BetBlitz/urls.py
+++ BetLux/BetBlitz/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . views import freeList, VIPList, BestOffersList
 from . import views
 urlpatterns = [
-    path('',views.homeView, name='home'),
+    path('', views.homeView, name='home'), 
     path('free/', freeList.as_view(), name='free'),
-    path('VIP',VIPList.as_view(), name='vip' ),
-    path('offers',BestOffersList.as_view(), name='offers' )
+    path('VIP/', VIPList.as_view(), name='vip'),     
+    path('offers/', BestOffersList.as_view(), name='offers'), 
 
   
 ]
--- BetLux/BetLux/urls.py
+++ BetLux/BetLux/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('BetBlitz.urls')),
+    path('bet/', include('BetBlitz.urls')),
 ]
